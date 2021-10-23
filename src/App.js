878b5e7a533af1717ca7274af3604c4c88ce559d
Ganesh <ganesh.birajdar.1930@gmail.com>
2021-10-23 13:22:31 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Container } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+//importing components
+import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
+
+//redux
+import { Provider } from "react-redux";
+import store from "./store";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Container fluid>
+        <Todo />
+        <TodoForm />
+      </Container>
+    </Provider>
+  );
+};
+
+export default App;
